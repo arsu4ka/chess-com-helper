64a4bf0409953494c8e3eb87e6bdfc01e5271c8d
Arsenii H. <origingoy@gmail.com>
2026-03-24 02:27:16 +0200

    fixed postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
-export const plugins = {
-	"postcss-import": {},
-	"@tailwindcss/postcss": {},
-	autoprefixer: {},
+module.exports = {
+	plugins: {
+		"postcss-import": {},
+		"@tailwindcss/postcss": {},
+		autoprefixer: {},
+	},
 };
